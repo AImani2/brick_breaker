7b59b4603fa42ad078ab0718495269b7cd269fb0
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-10-31 21:00:49 -0400

    model

---
+++ src/main/java/bwi/brick_breaker/BrickBreakerModel.java
@@ -0,0 +1,24 @@
+package bwi.brick_breaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrickBreakerModel {
+
+    // How to do this.
+    // Bricks are a list
+    // should bricks be 1 or 0?
+    // change brick to 0 when it is hit?
+    // only the 1 bricks will show
+    // when all bricks are 0 then the game is over
+
+    private Ball ball;
+    private List<Brick> bricks;
+    // or should we just do this with an array?
+    private int[] bricksV2;
+    private Panel panel;
+
+    // what methods do I need here?
+
+
+}
