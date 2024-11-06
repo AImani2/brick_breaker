7f58a8fe108a1d62094b89b02d9bbddd664d4faa
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-11-06 18:59:05 -0500

    checkpoint commit

--- src/main/java/bwi/brick_breaker/GameFrame.java
+++ src/main/java/bwi/brick_breaker/GameFrame.java
@@ -28,7 +28,7 @@ public class GameFrame extends JFrame {
         paddle = new Paddle(350, 550, 20, 100, Color.MAGENTA);
         int x = (int)paddle.getX() + ((int)paddle.getWidth() / 2) - 10;
         int y = (int) paddle.getY() - 20;
-        ball = new Ball(45, 5, x, y, 20, Color.CYAN);
+        ball = new Ball(45, 3, x, y, 20, Color.CYAN);
 
         BrickBreakerModel model = new BrickBreakerModel(ball, bricks, panel);
         Controller controller = new Controller(ball, paddle, bricks, model, view);
