414c3272a67b1ed547f1e39183903bfae2d2e5e1
rbral <rbral@student.touro.edu>
2024-12-10 15:56:42 -0500

    still not aiming well

--- src/main/java/bwi/brickbreaker/Simulation.java
+++ src/main/java/bwi/brickbreaker/Simulation.java
@@ -70,7 +70,7 @@ public class Simulation
     }
 
     public void movePaddle(NeuralNetwork neuralNetwork) {
-        double[] input = { ball.getCenterX(), paddle.getCenterX(), brick.getCenterX(), brick.getCenterY() };
+        double[] input = { ball.getX(), ball.getY(), brick.getCenterX(), brick.getCenterY() };
         double[] output = neuralNetwork.guess(input);
 
         if (output[0] > output[1]) {
@@ -165,12 +165,12 @@ public class Simulation
         paddle.setValY((int) paddle.getInitialY());
         paddle.setValX((int) (Math.random() * (width - paddle.getWidth()))); // Randomize paddle X position
 
-        //ball.setAngle(Math.random() > 0.5 ? 45 : 30);
+        ball.setAngle(Math.random() > 0.5 ? 45 : 30);
         ball.setX(paddle.getX() + (paddle.getWidth() / 2) - (ball.getWidth() / 2));
         ball.setY(paddle.getY() - ball.getHeight());
-        ball.setDx(rand.nextInt(175));
-        ball.setDy(-1);
-        brick = brickFactory.newBrick();
+        /*ball.setDx(rand.nextInt(175));
+        ball.setDy(-1);*/
+        //brick = brickFactory.newBrick();
 
     }
 
